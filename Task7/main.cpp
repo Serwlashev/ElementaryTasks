2cddf1b468cd0e667ea5e02456a85ca2dc3be304
Sergei <serwlashew@gmail.com>
2020-12-15 10:49:33 +0200

    Make logic for the loong number

---
+++ Task7/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#include "DimenshionlessNumber.h"
+
+int main() {
+
+	ISXNumber::LoongNumber numb;
+	numb.EnterString("123456789012345678901234567890123456789012345678901234567890123343345678901234567890123");
+
+
+
+	return 0;
+}
